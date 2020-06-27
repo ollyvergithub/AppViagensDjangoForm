41ee6b094dae3006f0e7541b17e089a5c3fb54cf
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-27 16:40:45 -0300

    Instalando django-bootstrap-form, para melhorar usar automaticamente as tags bootstrap nos campos - {{ form|bootstrap }}

--- setup/settings.py
+++ setup/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'passagens',
     'tempus_dominus',
+    'bootstrapform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
