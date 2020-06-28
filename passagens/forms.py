e3d78bdb4678cd09a1b971125143b94cf673d22a
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-28 09:10:42 -0300

    Passando de de forms.Form para forms.ModelForm no arquivo forms.py

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,26 +1,24 @@
 from builtins import list
-
 from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classes
 from passagens.validation import *
+from passagens.models import Passagem, ClasseViagem, Pessoa
 
 
-class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label="Destino", max_length=100)
-    data_ida = forms.DateField(label="Ida", widget=DatePicker())
-    data_volta = forms.DateField(label="Volta", widget=DatePicker())
+class PassagemForms(forms.ModelForm):
     data_pesquisa = forms.DateField(label="Data da Pesquisa", disabled=True, initial=datetime.today)
-    classe_viagem = forms.ChoiceField(label="Classe do vôo", choices=tipos_de_classes)
-    email = forms.EmailField(label="Email", max_length=150)
-    informacoes = forms.CharField(
-        label="Informações extras",
-        max_length=200,
-        widget=forms.Textarea,
-        required=False
-    )
+
+    class Meta:
+        model = Passagem
+        fields = '__all__'
+        labels = {'data_ida': 'Data de ida', 'data_volta': 'Data de volta', 'classe_viagem':'Classe do vôo',  'informacoes': 'Informações'}
+        widgets = {
+            'data_ida' : DatePicker(),
+            'data_volta': DatePicker(),
+        }
+
 
     def clean(self):
         origem = self.cleaned_data.get('origem')
@@ -34,7 +32,7 @@ class PassagemForms(forms.Form):
         campo_tem_algum_numero(destino, 'destino', lista_de_erros)
         origem_destino_iguais(origem, destino, lista_de_erros)
         data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros)
-        data_ida_menor_que_hoje(data_ida, data_pesquisa, lista_de_erros)
+        # data_ida_menor_que_hoje(data_ida, data_pesquisa, lista_de_erros)
 
         if lista_de_erros is not None:
             for erro in lista_de_erros:
